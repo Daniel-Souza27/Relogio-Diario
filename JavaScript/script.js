a55086d25f174998be79b10c62556c426741a973
Daniel Souza Filho <danieldsfilho@gmail.com>
2021-10-25 13:48:27 -0300

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,8 +2,8 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 22
+    var hora = data.getHours()
+    
     msg.innerHTML = `<h3> Agora são ${hora} horas.</h3>`
 
     if (hora >= 0 && hora < 12) {
