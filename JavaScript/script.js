70be8d22690e20a97ea8af07d6fa9e1badf40590
Daniel Souza Filho <danieldsfilho@gmail.com>
2021-10-26 17:06:07 -0300

    Versão 1.1
    Add mais opções de imagens;
    Add mais cores.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,23 +2,30 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-     var hora = data.getHours()
+    //  var hora = data.getHours()
+    var hora = 5
    
     
     msg.innerHTML = `<h3> Agora são ${hora} horas.</h3>`
 
-    if (hora >= 6 && hora < 12) {
+    if(hora >= 6 && hora < 12) {
         img.src = 'Images/manha.png'
         document.body.style.background = '#B3D2E0'
-    }else if (hora >= 12 && hora <= 16) {
-        img.src = ''
-        document.body.style.background = ''
-    }else if () {
-        img.src = ''
-        document.body.style.background = ''
-    } else{
-        img.src = '' 
-        document.body.style.background = ''
+    }else if(hora >=12 && hora < 15){
+        img.src = 'Images/ensolarado.png'
+        document.body.style.background = '#EAEC00'
+    }else if(hora >= 15 && hora <= 16) {
+        img.src = 'Images/iniciotarde2.png'
+        document.body.style.background = '#F4B676'
+    }else if(hora >= 17 && hora < 19 ){
+        img.src = 'Images/tarde.png' 
+        document.body.style.background = '#D85802'
+    }else if(hora >= 19 ){
+        img.src = 'Images/noite.png' 
+        document.body.style.background = '#103235'
+    }else{
+        img.src = 'Images/noite1.png'
+        document.body.style.background = '#13262F'
     }
 
 }
